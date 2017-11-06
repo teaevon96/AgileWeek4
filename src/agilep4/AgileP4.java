59d86db7e9267e70198f0965c2daf34c5c9e5b13
taruc <taruc@B5>
2017-11-06 12:32:06 +0800

    commitDone

--- src/agilep4/AgileP4.java
+++ src/agilep4/AgileP4.java
@@ -16,6 +16,7 @@ public class AgileP4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello Agile????");
     }
     
 }
